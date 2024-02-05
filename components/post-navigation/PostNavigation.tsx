3bcebcb7c23781287a2aaf3df0142fe46defed77
sunhoh <dltjsgho94@gmail.com>
2024-02-05 17:43:39 +0900

    fix: post navigation

--- components/post-navigation/PostNavigation.tsx
+++ components/post-navigation/PostNavigation.tsx
@@ -7,13 +7,13 @@ const PostNavigation = ({ prevPost, nextPost }:PostNavigationProps) => {
         <div className='flex items-center justify-between text-sm'>
           {!prevPost && !nextPost && <div className='h-[68px]' />}
           {prevPost ? (
-            <Link href={`/post/${prevPost.postId}`} className='flex flex-col max-w-[60%] tracking-tight hover:brightness-125 '>
+            <Link href={`/posts/${prevPost.postId}`} className='flex flex-col max-w-[60%] tracking-tight hover:brightness-125 '>
                 <div className='mb-2 font-light text-gray-400'>Prev</div>
                 <span>{prevPost.title}</span>
             </Link>
           ): <div />}
           {nextPost ? (
-            <Link href={`/post/${nextPost.postId}`} className='flex flex-col ml-auto max-w-[60%] tracking-tight text-right hover:brightness-125'>
+            <Link href={`/posts/${nextPost.postId}`} className='flex flex-col ml-auto max-w-[60%] tracking-tight text-right hover:brightness-125'>
                 <div className='mb-2 font-light text-gray-400'>Next</div> 
                 <span>{nextPost.title}</span>
             </Link>
